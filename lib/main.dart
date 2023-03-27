682018f118821c8293fdbaa8018af6b9ba9177a0
Michael Shimeles <michaelshimeles099@gmail.com>
2023-03-27 08:46:19 +0300

    some added transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 class HomePage extends StatelessWidget {
+  List<Transaction> transaction =[
+    Transaction( id:"001" ,name:"school fee",expense:999.99, date:DateTime.now()),
+    Transaction( id:"002" ,name:"rent",expense:899.09, date:DateTime.now()),
+    Transaction( id:"003" ,name:"Netflix ",expense:19.99, date:DateTime.now())];
+  
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
---
+++ lib/transaction.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class Transaction {
+  final String? id;
+  final String? name;
+  final double? expense;
+  final DateTime? date;
+  Transaction({this.id, this.name, this.expense, this.date});
+}
