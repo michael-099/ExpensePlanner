aa45fbaa2d91319460c2766be7c96325ebc34f83
Michael Shimeles <michaelshimeles099@gmail.com>
2023-03-30 13:22:07 +0300

    structure of the statless

---
+++ lib/widgets/transaction.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class transaction extends StatefulWidget{
+  transactionState createState()=>transactionState();
+}
+class transactionState extends State<transaction>{
+  Widget build (BuildContext context){
+    return Column();
+  }
+
+}
